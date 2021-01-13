52124e50deb14d8a41e94f4567fcac739493ed54
Ram Sah <mohan2036@gmail.com>
2021-01-13 01:11:39 -0500

    add test practice

--- algorithm.js
+++ algorithm.js
@@ -18,3 +18,37 @@ function findMinNum(arr){
   const result = findMinNum([10,1,3,6,2,3])
   console.log(result)
   
+  console.log("test questions ===================================")
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+//======================================================================
+
+
+//   var twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         const comp = target - nums[i];
+//         if (nums.indexOf(comp) !== -1 && nums.indexOf(comp) !== i) {
+//             return [i, nums.indexOf(comp)];
+//         }
+//     }
+//     return 'No complement';
+// };
+// console.log(twoSum([2,7,11,15],9))
+
+//or
+
+var twoSum = function(nums, target) {
+  for (var i=0; i<nums.length; i++) {
+      if (nums.slice(i+1,).indexOf(target - nums[i]) > -1){
+        
+          return [i, i+1+nums.slice(i+1,).indexOf(target - nums[i])];
+          }
+    }
+  }
+  console.log(twoSum([2,7,11,15], 18))
