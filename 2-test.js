10e3c1b50d11e2693c07afdfc1facaaf19b838d8
Ram Sah <mohan2036@gmail.com>
2021-02-23 23:39:59 -0500

    practice

--- 2-test.js
+++ 2-test.js
@@ -237,7 +237,6 @@ console.log("====================================24.")
 //Array.prototype.slice() or similar so that arr is a sub-array of the original array source with 
 //the first two elements omitted.
 
-
 const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function removeFirstTwo(list) {
   
