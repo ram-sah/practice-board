d3313403477b814b79cc6b4c6155f23734d476a6
Ram Sah <mohan2036@gmail.com>
2021-02-25 15:31:15 -0500

    practice

--- practice-1.js
+++ practice-1.js
@@ -123,3 +123,15 @@ for (i = 0; i < stuff.length; i++) {
 };
 console.log("stuff : " + stuff);
 console.log(randomValue + " is the random number");
+
+console.log("======================================")
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+  
+  const [a, b, ...arr] = list;
+
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
